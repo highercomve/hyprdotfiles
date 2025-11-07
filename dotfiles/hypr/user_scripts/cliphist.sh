8d77c0baba6f5042495e1d17d1674cb9faac8839
Sergio Marin <higher.vnf@gmail.com>
2025-11-07 09:08:58 -0300

    update cliplist functions

--- dotfiles/hypr/user_scripts/cliphist.sh
+++ dotfiles/hypr/user_scripts/cliphist.sh
@@ -8,17 +8,17 @@
 #
 
 case $1 in
-    d)
-        cliphist list | rofi -dmenu -config ~/.config/rofi/config-cliphist.rasi | cliphist delete
-        ;;
+d)
+    cliphist list | rofi -dmenu -config ~/.config/rofi/config-cliphist.rasi | cliphist delete
+    ;;
 
-    w)
-        if [ "$(echo -e "Clear\nCancel" | rofi -dmenu -config ~/.config/rofi/config-short.rasi)" == "Clear" ]; then
-            cliphist wipe
-        fi
-        ;;
+w)
+    if [ "$(echo -e "Clear\nCancel" | rofi -dmenu -config ~/.config/rofi/config-short.rasi)" == "Clear" ]; then
+        cliphist wipe
+    fi
+    ;;
 
-    *)
-        cliphist list | rofi -dmenu -config ~/.config/rofi/config-cliphist.rasi | cliphist decode | wl-copy
-        ;;
+*)
+    cliphist list | rofi -dmenu -config ~/.config/rofi/config-cliphist.rasi | cliphist decode | wl-copy
+    ;;
 esac
